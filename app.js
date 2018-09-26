c16875ba97fe3a55482d9392068e4bb51cae5a38
Or Goldfus <orgoldfus@Ors-MacBook-Pro.local>
2018-09-26 16:18:10 +0300

    remove unneeded files and handle async errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+require('express-async-errors');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const dbConnectionString = `${process.env.DB_HOST}/${process.env.DB_NAME}`
@@ -17,7 +17,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
@@ -28,13 +27,10 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const errorMsg = req.app.get('env') === 'development' 
+    ? err.message : 'An error was accured';
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send(errorMsg)
 });
 
 module.exports = app;
